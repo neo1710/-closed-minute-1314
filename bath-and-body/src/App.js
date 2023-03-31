7f539eed2c1151f433d293e7e73e5c8cfcc3e02b
neo1710 <neeraj3dworld@gmail.com>
2023-03-31 11:21:41 +0530

    ko

--- bath-and-body/src/App.js
+++ bath-and-body/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/nav';
+import Body from './components/body';
 
 function App() {
   return (
     <div className="App">
     <Nav/>
+    <Body/>
     </div>
   );
 }
---
+++ bath-and-body/src/components/body.jsx
@@ -0,0 +1,14 @@
+import { Box ,Image} from "@chakra-ui/react";
+
+export default function Body(){
+
+   return (
+    <Box>
+        <Box>
+            <Image 
+            src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw456b9c80/images/Spring2023/sp_wavesofwellness_sp3_hm.jpg?yocs=s_"
+             alt="not found"/>
+        </Box>
+    </Box>
+   ) 
+}
